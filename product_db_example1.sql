338d880d3d4a73e9322823e424bc424fd45d59d6
G. Willson <greg@biomassiv.es>
2016-07-12 14:50:19 -0600

    Create product_db_example1.sql

---
+++ product_db_example1.sql
@@ -0,0 +1,45 @@
+7/12/16 2:50:00 PM Text Compare Page 1 Mode: Differences
+Left file: /Users/geog/Downloads/prestashop (3).sql Right file: /Users/geog/Downloads/prestashop (2).sql
+      
+(31, 698, 1, NULL, 0, 0, 'after', '.dash_news', 0 » , 0, 1),
+ 
+<>
+(31, 698, 1, NULL, 1, 0, 'after', '.dash_news', 0 » , 0, 1),
+(154, NULL, NULL, 'PS_PACK_FEATURE_ACTIVE', NULL » , '0000-00-00 00:00:00', '2016-07-12 08:57:47' » ),
+ 
+ 
+<>
+(154, NULL, NULL, 'PS_PACK_FEATURE_ACTIVE', NULL » , '0000-00-00 00:00:00', '2016-07-12 08:46:00' » ),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCR » EMENT=11 ;
+ 
+<>
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCR » EMENT=10 ;
+(9, 1, 0, 'Product modification', 'Product', 1, 1 » , '2016-07-12 08:46:00', '2016-07-12 08:46:00' » ),
+(10, 1, 0, 'Product modification', 'Product', 1, » 1, '2016-07-12 08:57:47', '2016-07-12 08:57:4 » 7');
+ 
+ 
+<>
+(9, 1, 0, 'Product modification', 'Product', 1, 1 » , '2016-07-12 08:46:00', '2016-07-12 08:46:00' » );
+(1, 1, 1, 5, 1, 1, 0, 0, '0', '', 0.000000, 0, 1, » 16.510000, 4.950000, '', 0.000000, 0.00, 'demo » _1', '', '', 0.000000, 0.000000, 0.000000, 0.0 » 00000, 2, 0, 0, 0, 0, 1, '404', 0, 0, '0000-00- » 00', 'new', 0, 1, 'both', 0, 0, 0, 1, '2016-07- » 11 18:35:42', '2016-07-12 08:57:47', 0, 3),
+ 
+ 
+ 
+ 
+ 
+<>
+(1, 1, 1, 5, 1, 1, 0, 0, '0', '', 0.000000, 0, 1, » 16.510000, 4.950000, '', 0.000000, 0.00, 'demo » _1', '', '', 0.000000, 0.000000, 0.000000, 0.0 » 00000, 2, 0, 0, 0, 0, 1, '404', 0, 1, '0000-00- » 00', 'new', 1, 1, 'both', 0, 0, 0, 1, '2016-07- » 11 18:35:42', '2016-07-12 08:46:00', 0, 3),
+(1, 1, 5, 1, 0, 0, 0.000000, 1, 16.510000, 4.9500 » 00, '', 0.000000, 0.00, 0, 0, 0, 1, '404', 0, 0, » '0000-00-00','new',0,1,'both',1,0,'201 » 6-07-11 18:35:42', '2016-07-12 08:57:47', 3),
+ 
+ 
+ 
+<>
+(1, 1, 5, 1, 0, 0, 0.000000, 1, 16.510000, 4.9500 » 00, '', 0.000000, 0.00, 0, 0, 0, 1, '404', 0, 1, » '0000-00-00','new',1,1,'both',1,0,'201 » 6-07-11 18:35:42', '2016-07-12 08:46:00', 3),
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCR » EMENT=1085 ;
+ 
+<>
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCR » EMENT=1010 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCR » EMENT=9 ;
+ 
+<>
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCR » EMENT=8 ;
+Beyond Compare v4.1.3
